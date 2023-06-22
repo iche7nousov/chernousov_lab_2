632e8e21d1281a7e3ebf3851805faa13f9b4896d
iche7nousov <137150172+iche7nousov@users.noreply.github.com>
2023-06-22 15:12:51 +0300

    Add files via upload

---
+++ numbers.py
@@ -0,0 +1,23 @@
+
+"""
+Запросите у пользователя четыре числа (файл numbers.py). Отдельно сложите первые два и
+отдельно вторые два. Разделите первую сумму на вторую. Выведите результат на экран так,
+чтобы ответ содержал две цифры после запятой.
+"""
+def numbers():
+    # Запишим значение в память
+    one = float(input("1: "))
+    two = float(input("2: "))
+    three = float(input("3: "))
+    four = float(input("4: "))
+
+    first_sum = one + two
+    last_sum = three + four
+    answer = first_sum / last_sum
+    print("%.2f" % answer)
+    input("")
+
+
+# Если объект __name__ равен __main__ запускаем наш код
+if __name__ == "__main__":
+    numbers()
---
+++ user.py
@@ -0,0 +1,38 @@
+
+def get_your_live(name, age):
+    # Запишим введеный текст в память
+    your_live = str(input("Where are you live: "))
+    # Проверим - состоит ли строка только из букв
+    if your_live.isalpha():
+        # Выведем текст в консоль
+        print(f"This is {name}\n"
+              f"It is {age}\n"
+              f"(S)he live in {your_live}")
+        input("")
+        # Если есть другие символы - вернем к вводу текста
+    else:
+        get_your_live(name, age)
+def get_your_age(name):
+    # Запишим введеный текст в память
+    your_age = str(input("How old are you: ")) # Тип памяти str для проверки isdigit
+    # Проверим - состоит ли строка только из цифр и возраст больше 1 и меньше 120
+    if your_age.isdigit() and int(your_age) >= 1 and int(your_age) <= 120:
+        # Переходим к вводу места жительства и перетаскиваем значение текста
+        get_your_live(name, int(your_age))
+    # Если есть другие символы - вернем к вводу текста
+    else:
+        get_your_age(name)
+def get_your_name():
+    # Запишим введеный текст в память
+    your_name = str(input("What is your name: "))
+    # Проверим - состоит ли строка только из букв
+    if your_name.isalpha():
+        # Переходим к вводу возраста и перетаскиваем значение текста
+        get_your_age(your_name)
+    # Если есть другие символы - вернем к вводу текста
+    else:
+        get_your_name()
+
+# Если объект __name__ равен __main__ запускаем наш код
+if __name__ == "__main__":
+    get_your_name()
